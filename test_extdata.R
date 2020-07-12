8977d0919b3b9b984966c6f9fd45fcb5ec6734ba
ssshan152212 <68190520+ssshan152212@users.noreply.github.com>
2020-07-13 01:06:30 +0530

    Create test_extdata.R

---
+++ test_extdata.R
@@ -0,0 +1,8 @@
+test_that(".csv data files are available", {
+    testthat::expect_equal(
+        list.files(system.file("extdata", package = "FARSfun")),
+        c("accident_2013.csv.bz2",
+          "accident_2014.csv.bz2",
+          "accident_2015.csv.bz2"))
+    }
+)
